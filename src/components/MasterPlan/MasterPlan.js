1462ac017be6d6fbc48e27ab7b2e94826f8f79cf
Francisco Campos <franciscocampos@Franciscos-MacBook-Air.local>
2021-12-01 05:29:59 -0600

    add fondo4

--- src/components/MasterPlan/MasterPlan.js
+++ src/components/MasterPlan/MasterPlan.js
@@ -1,18 +1,20 @@
 import { Col, Row } from 'react-bootstrap'
 import PlanMasterImg from '../../img/masterPlan.png'
+import Fondo4 from '../../img/fondo4.png'
+
 import './Style.css'
 
 const MasterPlan = ({ lg }) => {
   return (
     <Col lg={lg} className="master-container bg-dark text-light">
       <Row>
-        <Col>
+        <Col className="master-grid">
           <h3>12 AMENIDADES</h3>
         </Col>
-        <Col>
+        <Col className="master-grid">
           <h3>60% AREA VERDE</h3>
         </Col>
-        <Col>
+        <Col className="master-grid">
           <h3>SERVICIO DE INTERNET DE FIBRA OPTICA </h3>
         </Col>
       </Row>
@@ -23,6 +25,11 @@ const MasterPlan = ({ lg }) => {
             src={PlanMasterImg}
             className="d-inline-block align-top"
           />
+        </Col>
+      </Row>
+      <Row>
+        <Col className="fondo4">
+          <img alt="" src={Fondo4} className="d-inline-block align-top" />
         </Col>
       </Row>
     </Col>
